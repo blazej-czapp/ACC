babb9e55d5a25361f92cb952987bb2c80e140690
Blazej Czapp <bczapp@gmail.com>
2014-08-30 15:20:39 +0100

    Added licence statement

--- src/Array.hpp
+++ src/Array.hpp
@@ -1,3 +1,25 @@
+// The MIT License (MIT)
+
+// Copyright (c) 2014 Błażej Czapp, Damian Powązka
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #ifndef ACC_ARRAY
 #define ACC_ARRAY
 
@@ -14,8 +36,8 @@ public:
     }
 
     Array(const Array& other) : m_size(other.m_size), m_capacity(other.m_capacity) {
-    	allocStorage(m_capacity);
-    	std::copy(other.m_content, other.m_content + other.m_capacity, m_content);
+        allocStorage(m_capacity);
+        std::copy(other.m_content, other.m_content + other.m_capacity, m_content);
     }
 
     virtual ~Array<T>() {
@@ -26,7 +48,7 @@ public:
         return m_content[index];
     }
 
-	const T& operator[](size_t index) const {
+    const T& operator[](size_t index) const {
         return m_content[index];
     }
 
@@ -61,7 +83,7 @@ public:
 
 private:
     void allocStorage(size_t size) {
-		m_content = new T[size];
+        m_content = new T[size];
     }
 
     T* m_content;
@@ -69,6 +91,6 @@ private:
     size_t m_capacity;
 };
 
-}
+} // namespace acc
 
 #endif // ACC_ARRAY
